1bb68ceca6bdaaf2e73c47065e717cfe283df3f4
leila bahta <overflowngrace@gmail.com>
2021-03-03 19:13:56 -0600

    (first commit)

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Jeep from './Jeep';
+import Honda from './Honda';
+import Toyota from './Toyota';
+import Ford from './Ford';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Cars</h1>
+      <Jeep/>
+      <Honda/>
+      <Toyota/>
+<Ford/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Jeep.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import './main.css';
+
+class Jeep extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            count:this.props.startingCount
+        }
+    this.add=this.add.bind(this);
+    this.minus=this.minus.bind(this);
+    this.reset=this.reset.bind(this);
+
+    }
+    add(){
+        this.setstate=({
+            count:this.state.count+1
+        })
+    }
+    minus(){
+        this.setstate=({
+            count:this.state.count-1
+        })
+    }
+    reset(){
+this.setstate=({
+    count:this.state.count
+})
+
+    }
+    componentDidMount(){
+      console.log("Mount");    }
+            componentDidUpdated(){
+                console.log("Updated"); 
+                 }
+    render(){
+    return(
+<div className="main" >
+    <h1>Jeep:</h1>
+<h1>{this.state.count}</h1>
+<button onClick={this.add}><h1>+</h1></button>
+<button onClick={this.minus}><h1>-</h1></button>
+<button onClick={this.reset}><h1>0</h1></button>
+
+    <Jeep/>
+</div>
+
+
+    )
+}
+}export default Jeep;
